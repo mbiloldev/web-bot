f92bdeae62189500d01bfcd3ba7342767b6758f4
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-14 13:33:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ from foykey import foymenu
 from config import BOT_TOKEN
 
 
-    await message.answer(
-        f"Assalomu alaykum {message.from_user.full_name} botimga tashrif buyurganingizdan bag'oyatda xursandman !",
-        reply_markup=foymenu),
-    if message.from_user.id == int(6182560423):
-        await message.answer_sticker('CAACAgIAAxkBAAMpZBAAAfUO9xqQuhom1S8wBMW98ausAAI4CwACTuSZSzKxR9LZT4zQLwQ')
-        await message.answer(f'Assalomu alaykum adminstrator Muhammadbilol botga xush kelibsiz !',
-                             reply_markup=adminmenu),
-
 
 @dp.message_handler(commands=['help'])
 async def help(message: types.Message):
