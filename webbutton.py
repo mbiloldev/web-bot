44e69bb2450ab5d56f3ec5edd4c474e29eb7e425
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-14 13:34:48 +0500

    Update webbutton.py

--- webbutton.py
+++ webbutton.py
@@ -1,14 +1,5 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-royhatdanotish = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(text="Web-site", url="https://qoradev.pythonanywhere.com/"),
-
-        ],
-
-    ])
-
 
 admin = InlineKeyboardMarkup(
     inline_keyboard=[
