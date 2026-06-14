0b6cd820c46a7fc0399ccd88a7df82ec891872f7
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-14 13:34:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,56 @@
-qsdwefrvbg
+import os
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ParseMode, Message
+
+import config
+from foykey import foymenu
+from keyboards import adminmenu
+from config import BOT_TOKEN
+from webbutton import royhatdanotish, admin
+
+bot = Bot(config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer(
+        f"Assalomu alaykum {message.from_user.full_name} botimga tashrif buyurganingizdan bag'oyatda xursandman !",
+        reply_markup=foymenu),
+    if message.from_user.id == int(6182560423):
+        await message.answer_sticker('CAACAgIAAxkBAAMpZBAAAfUO9xqQuhom1S8wBMW98ausAAI4CwACTuSZSzKxR9LZT4zQLwQ')
+        await message.answer(f'Assalomu alaykum adminstrator Muhammadbilol botga xush kelibsiz !',
+                             reply_markup=adminmenu),
+
+
+@dp.message_handler(commands=['help'])
+async def help(message: types.Message):
+    await message.answer("Buyruqlar:\n/start - Botni ishga tushirish\n/help - Yordam")
+
+
+@dp.message_handler(text="Ilovani yuklash 📲")
+async def send_apk(message: Message):
+    await message.answer_document('BQACAgIAAxkBAANTZg_ILpJtFKrqY5T5_U6NewgAATIWAAI7QgACpaVASKuWEly6brNENAQ',
+                                  caption='Bu ilova faqat Android uchun !', )
+
+
+@dp.message_handler(text="Admin-panel ilovasini yuklash ! 📲")
+async def send_apk(message: Message):
+    await message.answer_document('BQACAgIAAxkBAANVZg_IqiCmFz54vzg7Pxi8fHGMg4MAAvdHAAITD4FIvfIJsEoFiFI0BA',
+                                  caption='Bu ilova faqat Android uchun !', )
+
+
+@dp.message_handler(text='Saytni korish 💻')
+async def send_photo(message: Message):
+    await message.answer_photo('AgACAgIAAxkBAAIBR2YSWGGr5hFMCvonP-v1fzzlS1PrAAKm1TEbEw-BSOv2qBFWXQhjAQADAgADeQADNAQ',
+                               caption='Web-siteniochish uchun tugmani bosing 👇', reply_markup=royhatdanotish)
+
+
+@dp.message_handler(text="Admin Panelni ko'rish 💻")
+async def send_photo(message: Message):
+    await message.answer_photo('AgACAgIAAxkBAAIBR2YSWGGr5hFMCvonP-v1fzzlS1PrAAKm1TEbEw-BSOv2qBFWXQhjAQADAgADeQADNAQ',
+                               caption='Web-siteniochish uchun tugmani bosing 👇', reply_markup=admin)
+
+
+executor.start_polling(dp, skip_updates=True)
