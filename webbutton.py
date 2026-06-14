4e59205c83fc717d1aaa15b1dbc5fb1760567411
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-14 13:30:30 +0500

    Add files via upload

---
+++ webbutton.py
@@ -0,0 +1,20 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+royhatdanotish = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Web-site", url="https://qoradev.pythonanywhere.com/"),
+
+        ],
+
+    ])
+
+
+admin = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Web-site admin panelini ochish !", url="https://qoradev.pythonanywhere.com/admin/login/?next=/admin/"),
+
+        ],
+
+    ])
